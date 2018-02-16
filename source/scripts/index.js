e4c725e95a1ec9e640d95cdc5666a40d684bc25b
Kevin Tarwater <tarwaterk@gmail.com>
2018-02-15 20:00:54 -0600

    Attach click event listeners to game control buttons

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,23 @@
 import colorGenerator from './colorGenerator';
 
-let colors = colorGenerator(6);
-console.log(colors);
+window.onload = () => {
+    gameLogic.addEventListeners();
+}
+
+var gameLogic = {
+    addEventListeners: () => {
+        let newGameBtn = document.querySelector(".js-new-game-btn");
+        let difficultyBtns = document.querySelectorAll(".js-difficulty-btn");
+
+        newGameBtn.addEventListener("click", () => {
+            let colors = colorGenerator(6);
+            console.log(colors);
+        });
+
+        difficultyBtns.forEach((button) => {
+            button.addEventListener("click", () => {
+                console.log("difficulty button");
+            });
+        });
+    }
+}
